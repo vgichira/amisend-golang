0ed8dfcdb901a8d5ed2463af25c7ae4d138f74b2
Vincent Gichira <vgichira39@gmail.com>
2018-11-23 16:31:59 +0300

    send message to all contacts api

---
+++ sendmessagetoall/sendmessagetoall.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	// endpoint
+	var sendMessageURL string = "https://amisend.com/api/sms/send/all"
+
+	// authentication
+	var x_username string = "vgichira"
+	var x_apikey string = "vgichira"
+
+	// data
+	createMessage := map[string]string{
+		"message":  "Relax, this is a test",
+		"senderId": "",
+		"refId":    "amiami",
+	}
+
+	params, err := json.Marshal(createMessage)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// request
+	request, err := http.NewRequest("POST", sendMessageURL, bytes.NewBuffer(params))
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// headers
+	request.Header.Set("Content-Type", "application/json")
+	request.Header.Set("x-api-user", x_username)
+	request.Header.Set("x-api-key", x_apikey)
+
+	// response
+	response, err := http.DefaultClient.Do(request)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	body, err := ioutil.ReadAll(response.Body)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Println(string(body))
+}
