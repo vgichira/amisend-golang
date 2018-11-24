7abe29cc8de23302e16c912932efc3661c6e9472
Vincent Gichira <vgichira39@gmail.com>
2018-11-24 18:37:26 +0300

    fetch balance api

---
+++ fetchbalance/fetchbalance.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	// endpoint
+	var endPoint string = "https://amisend.com/api/topups/balance"
+
+	// authentication
+	var x_username string = "vgichira"
+	var x_apikey string = "vgichira"
+
+	// request
+	request, err := http.NewRequest("GET", endPoint, nil)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// headers
+	request.Header.Set("Content-Type", "application/json")
+	request.Header.Set("x-api-user", x_username)
+	request.Header.Set("x-api-key", x_apikey)
+
+	// response
+	response, err := http.DefaultClient.Do(request)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	body, err := ioutil.ReadAll(response.Body)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer response.Body.Close()
+	fmt.Println(string(body))
+}
